d4c2dce6db74af185526b54507c95fe5162d9698
zafar <zafarkhan808045@gmail.com>
2022-03-20 23:44:09 +0500

    srap data from tab;e

---
+++ index.js
@@ -0,0 +1,18 @@
+const request = require("request-promise");
+const cherio = require("cheerio");
+
+const scrapdata = async () => {
+  const result = await request.get(
+    "https://www.codingwithstefan.com/table-example/"
+  );
+  const $ = cherio.load(result);
+  //$('body > table > tbody > tr').each((a,b)=>{console.log( $(b).find("td")[0])})
+  //body > table > tbody > tr > td
+  const datas = $("body > table > tbody > tr").each((index, element) => {
+    const tds = $(element).find("td");
+    const company = $(tds[0]).text();
+    console.log(company);
+  });
+};
+
+scrapdata();
