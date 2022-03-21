4993ca330db9d5a6a67c7299e54f12c349cb4576
zafar <zafarkhan808045@gmail.com>
2022-03-21 17:08:12 +0500

    abcd

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ const scrapdata = async () => {
     "https://www.codingwithstefan.com/table-example/"
   );
   const $ = cherio.load(result);
+
   //$('body > table > tbody > tr').each((a,b)=>{console.log( $(b).find("td")[0])})
   //body > table > tbody > tr > td
+  const scrapRrows = [];
   const datas = $("body > table > tbody > tr").each((index, element) => {
     const tds = $(element).find("td");
     const company = $(tds[0]).text();
-    console.log(company);
-    
+    const contact = $(tds[1]).text();
+    const country = $(tds[2]).text();
+    const scraprows = { company, contact, country };
+    scrapRrows.push(scraprows);
   });
+  console.log(scrapRrows);
 };
 
 scrapdata();
